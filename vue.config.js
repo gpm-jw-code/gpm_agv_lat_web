4a777627b2a3f35bb0c95ec649680e1656cdebe2
jinwei69 <ss610349@gmail.com>
2022-08-31 09:58:29 +0800

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  pages:{
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'GPM LAT',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  }
 })
